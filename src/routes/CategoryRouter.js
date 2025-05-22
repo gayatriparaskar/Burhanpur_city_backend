00a3c4229b34e4f7e864244ae4bbe5ede6d2bd93
Gayatri Paraskar <gayatriparaskar133@gmail.com>
2025-05-22 11:12:40 +0530

    remove authentication in category section

--- src/routes/CategoryRouter.js
+++ src/routes/CategoryRouter.js
@@ -4,10 +4,10 @@ const authentication = require ("../middleware/authentication");
 const categoryRoutes = express.Router();
 const { createCategory , getCategory , updateCategory , deleteCategory , searchCategory} = require ("../controllers/CategoryController");
 
-categoryRoutes.post('/createCategory',authentication,createCategory);
+categoryRoutes.post('/createCategory',createCategory);
 categoryRoutes.get('/getCategory',getCategory);
-categoryRoutes.put('/updateCategory/:id',authentication,updateCategory);
-categoryRoutes.delete('/deleteCategory/:id',authentication,deleteCategory);
+categoryRoutes.put('/updateCategory/:id',updateCategory);
+categoryRoutes.delete('/deleteCategory/:id',deleteCategory);
 categoryRoutes.get('/searchCategory',searchCategory);
 
 module.exports = categoryRoutes ; 
