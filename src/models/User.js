ae2d3bc4f0f6d71c27115cae5d1d0e44f46c6bb7
Gayatri Paraskar <gayatriparaskar133@gmail.com>
2025-05-26 10:53:58 +0530

    phone number is unique in user creation time is added

--- src/models/User.js
+++ src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin',"owner"], default: 'user' },
-  phone: { type: String },
+  phone: { type: String, unique:true },
   address: { type: String },
   createdAt: { type: Date, default: Date.now },
   isActive: { type: Boolean, default: true }
